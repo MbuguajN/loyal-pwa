5db7fb83f9a4b80b91cc1ca6b85569f4d259f367
MbuguajN <jnjoroge444@gmail.com>
2025-02-16 23:27:41 +0300

    set-up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
+
+const withPWA = require('next-pwa')({
+  dest: 'public', // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker to activate
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+module.exports = withPWA({
+  // Your existing Next.js configuration
+  reactStrictMode: true,
+});
 
 export default nextConfig;
