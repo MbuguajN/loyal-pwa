b1dcd55f51d3cd810013216ce1dca4c0757e7c73
MbuguajN <jnjoroge444@gmail.com>
2025-02-17 11:34:25 +0300

    pwa fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#000000" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 
 const withPWA = require('next-pwa')({
   dest: 'public', // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  disable: false,
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker to activate
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
 };
 
 module.exports = withPWA({
