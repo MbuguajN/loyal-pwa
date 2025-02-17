7e464a9be769b8b39a2f2e01bb72176a0f27cd89
bradleygichuru <bradleygichuru@gmail.com>
2025-02-17 19:19:30 +0300

    feat:adds register user route

--- app/api/register-user/route.ts
+++ app/api/register-user/route.ts
@@ -1,4 +1,4 @@
-export async function POST(request: Request, response: Response) {
+export async function POST(request: Request) {
 
 	console.log(request.body)
 	return Response.json({ status: "user added" });
